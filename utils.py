a63d8bf0c4c4603d2909e984c0e731085966172a
alvinshaita <alvinshaita@gmail.com>
2024-04-01 19:29:31 +0300

    hash with sha256_hash

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import requests
 
 
@@ -5,3 +6,9 @@ import requests
 # a = '''“We have over 100,000 children, which we’ve never had before, in serious condition, and many on ventilators” due to the coronavirus.'''
 def fetch_from_url(url):
 	return url
+
+def sha256_hash(data):
+	sha256 = hashlib.sha256()
+	sha256.update(data.encode("utf-8"))
+	hashed_data = sha256.hexdigest()
+	return hashed_data
